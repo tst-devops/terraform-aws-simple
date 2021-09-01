4c9c8ca430a30f4d0f3b760841e0efb3a07ce1e9
Chan <anderson.chan@hal.hitachi.com>
2021-09-01 16:15:07 -0700

    added initial terraform file

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-west-1"
+}
+
+resource "aws_instance" "lab_app_server" {
+  ami           = "ami-0d382e80be7ffdae5"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "terraform-AWS-SimpleEC2"
+  }
+}
